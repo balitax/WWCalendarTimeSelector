11526ec8975e5cb9df410e421f38dcd418f7fd57
Agus Cahyono <cahyo.mamen@gmail.com>
2018-08-21 18:40:52 +0700

    create helpers group and new class public

---
+++ Sources/Helpers/WWCalendarTimeSelectorSelection.swift
@@ -0,0 +1,9 @@
+//
+//  WWCalendarTimeSelectorSelection.swift
+//  WWCalendarTimeSelector
+//
+//  Created by Agus Cahyono on 21/08/18.
+//  Copyright © 2018 Wonder. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/Helpers/WWCalendarTimeSelectorStyle.swift
@@ -0,0 +1,9 @@
+//
+//  WWCalendarTimeSelectorStyle.swift
+//  WWCalendarTimeSelector
+//
+//  Created by Agus Cahyono on 21/08/18.
+//  Copyright © 2018 Wonder. All rights reserved.
+//
+
+import Foundation
